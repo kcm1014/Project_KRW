64b4598cd51e112f205dfb44a9deb6465834826f
kcm1014 <kcm1014@naver.com>
2023-03-07 15:07:21 +0900

    20230307 complete

--- krwproject/rate/admin.py
+++ krwproject/rate/admin.py
@@ -18,12 +18,26 @@ class CategoryAdmin(admin.ModelAdmin):
 
 @admin.register(SubCategory)
 class SubCategoryAdmin(admin.ModelAdmin):
-    list_display = ['id', 'subcategory_order', 'subcategory_name', 'subcategory']
+    list_display = ['id', 'subcategory_order', 'subcategory_name', 'subcategory','category_name']
+    def category_name(self, obj):
+        if obj.subcategory:
+            return obj.subcategory.category_name
 
+    category_name.short_description = "Category Name"
 @admin.register(RateContent)
 class RateContentAddmin(admin.ModelAdmin):
-    list_display = ['id','category','subcategory','isApproval','create_date', 'point01', 'point02','point03','point04','point05','point06','contents','userId','userPwd','schPwd']
+    list_display = ['id','category_name','subcategory_name','isApproval','create_date', 'point01', 'point02','point03','point04','point05','point06','contents','userId','userPwd','schPwd']
 
+    def category_name(self, obj):
+        if obj.category:
+            return obj.category.category_name
+
+    def subcategory_name(self, obj):
+        if obj.subcategory:
+            return obj.subcategory.subcategory_name
+
+    category_name.short_description="Category Name"
+    subcategory_name.short_description ="Subcategory Name"
 @admin.register(SchoolPwd)
 class SchoolPwdAdmin(admin.ModelAdmin):
     list_display = ['id', 'create_date', 'schPwd']
